2cc930c8cdab760c8573c4941a420dccacdeec62
wuhuikai <huikaiwu@icloud.com>
2017-09-16 21:14:37 +0800

    Add scripts for sampling replays

--- sample_actions.py
+++ sample_actions.py
@@ -21,20 +21,19 @@ flags.DEFINE_string(name='parsed_replays', default='parsed_replays',
                     help='Path for parsed actions')
 flags.DEFINE_string(name='infos_path', default='replays_infos',
                     help='Paths for infos of replays')
-flags.DEFINE_integer(name='step_mul', default=96,
+flags.DEFINE_integer(name='step_mul', default=8,
                      help='step size')
+flags.DEFINE_integer(name='skip', default=96,
+                     help='# of skipped frames')
 
-def sample_action_from_player(global_info_path, action_path):
-    with open(global_info_path) as f:
-        global_info = json.load(f)
-    feat = features.Features(Parse(global_info['game_info'], sc_pb.ResponseGameInfo()))
-
+def sample_action_from_player(action_path):
+    feat = features.Features(screen_size_px=(1, 1), minimap_size_px=(1, 1))
     with open(action_path) as f:
         actions = json.load(f)
 
     frame_id = 0
     result_frames = []
-    for step_mul, action_strs in actions:
+    for action_strs in actions:
         action_name = None
         for action_str in action_strs:
             action = Parse(action_str, sc_pb.Action())
@@ -46,15 +45,14 @@ def sample_action_from_player(global_info_path, action_path):
             if func_name.split('_')[0] in {'Build', 'Train', 'Research', 'Morph', 'Cancel', 'Halt', 'Stop'}:
                 action_name = func_name
                 break
-        if action_name is not None or (frame_id > 0 and frame_id%FLAGS.step_mul == 0):
-            result_frames.append(frame_id-step_mul)
+        if frame_id > 0 and (action_name is not None or frame_id%FLAGS.skip == 0):
+            result_frames.append(frame_id-FLAGS.step_mul)
 
-        frame_id += step_mul
+        frame_id += FLAGS.step_mul
 
     return result_frames
 
-
-def sample_action(replay_path, global_info_path, action_path, sampled_path):
+def sample_action(replay_path, action_path, sampled_path):
     replay_info = os.path.join(FLAGS.infos_path, replay_path)
     if not os.path.isfile(replay_info):
         return
@@ -66,20 +64,16 @@ def sample_action(replay_path, global_info_path, action_path, sampled_path):
     for p in proto.player_info:
         player_id = p.player_info.player_id
         race = sc_pb.Race.Name(p.player_info.race_actual)
-        global_info = os.path.join(global_info_path, race, 'G_{}_{}'.format(player_id, replay_path))
-        if not os.path.isfile(global_info):
-            return
-        action = os.path.join(action_path, race, 'A_{}_{}'.format(player_id, replay_path))
-        if not os.path.isfile(action):
+
+        action_file = os.path.join(action_path, race, '{}@{}'.format(player_id, replay_path))
+        if not os.path.isfile(action_file):
             return
 
-        result.append(sample_action_from_player(global_info, action))
+        result.append(sample_action_from_player(action_file))
 
     assert len(result) == 2
-    player_1, player_2 = result
-
-    sampled_actions = sorted(set(player_1) | set(player_2))
-
+    sampled_actions = sorted(set(result[0]) | set(result[1]))
+    
     with open(os.path.join(sampled_path, replay_path), 'w') as f:
         json.dump(sampled_actions, f)
 
@@ -89,18 +83,14 @@ def main():
     replay_list = sorted([p for p, _ in replay_list])
 
     race_vs_race = os.path.basename(FLAGS.hq_replay_set).split('.')[0]
-    FLAGS.parsed_replays = os.path.join(FLAGS.parsed_replays, race_vs_race)
-
-    sampled_path = os.path.join(FLAGS.parsed_replays, 'SampledActions')
+    sampled_path = os.path.join(FLAGS.parsed_replays, 'SampledActions', race_vs_race)
     if not os.path.isdir(sampled_path):
         os.makedirs(sampled_path)
-
-    global_info_path = os.path.join(FLAGS.parsed_replays, 'GlobalInfos')
-    action_path = os.path.join(FLAGS.parsed_replays, 'Actions')
+    action_path = os.path.join(FLAGS.parsed_replays, 'Actions', race_vs_race)
 
     pbar = tqdm(total=len(replay_list), desc='#Replay')
     for replay_path in replay_list:
-        sample_action(os.path.basename(replay_path), global_info_path, action_path, sampled_path)
+        sample_action(os.path.basename(replay_path), action_path, sampled_path)
         pbar.update()
 
 if __name__ == '__main__':
